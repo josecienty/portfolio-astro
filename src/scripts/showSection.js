867c0fbea3863f97475b3f932bd5e9db9b833a1c
José Argüello <argelloestigarribia.josantonio@gmail.com>
2025-04-10 11:10:17 -0300

    pagina de presentacióncon efecto matrix

--- src/scripts/showSection.js
+++ src/scripts/showSection.js
@@ -2,13 +2,32 @@ let currentSection = 0;
 let isScrolling = false;
 
 const sections = document.querySelectorAll("section");
+//? Para topBar
+const styleActive = 'bg-white/15';
+const topbar = document.querySelector("#topbar");
 
-document.documentElement.scrollIntoView({
-    top: 0
+//? Si no tiene hash ir siempre arriba
+if (!document.location.hash?.length) {
+    document.documentElement.scrollIntoView({ top: 0 });
+} else {
+    const index = Array.from(sections).findIndex(
+        item => '#' + item.id === document.location.hash && item.classList.contains('fullpage')
+    );
+    // Se registra en el current index para que
+    // Funcione los eventos al cambio de hash
+    if (index >= 0) currentSection = index;
+}
+
+//? Para no mostrar #inicio
+window.addEventListener('hashchange', function (a) {
+    if (document.location.hash == '#inicio') {
+        history.replaceState(null, '', '/');
+        scrollToSection(0);
+    }
 });
 
 function scrollToSection(index) {
-   if (index >= 0 && index < sections.length) {
+    if (index >= 0 && index < sections.length) {
         if (index == 0) {
             document.documentElement.scrollIntoView({
                 top: 0,
@@ -19,7 +38,7 @@ function scrollToSection(index) {
         }
         setTimeout(() => {
             isScrolling = false;
-        },200);
+        }, 200);
     }
 }
 
@@ -31,36 +50,40 @@ function getAction(event) {
     }
 }
 
-function calcularSeccion(){
+function calcularSeccion() {
     sections.forEach((section, index) => {
-        if(section.getBoundingClientRect()) currentSection=index;
+        if (section.getBoundingClientRect()) currentSection = index;
     })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const index = Array.from(sections).indexOf(entry.target); // Obtén el índice de la sección
-          if (entry.isIntersecting && !entry.target.classList.contains('fullpage')) {
-            currentSection = index;
-          }
-        });
-      },
-      {
-        threshold: 0.8, 
-      }
+        (entries) => {
+            entries.forEach((entry) => {
+                const index = Array.from(sections).indexOf(entry.target); // Obtén el índice de la sección
+
+                //? Esto es para activar el topbar
+                topbar.children[index].classList.toggle(styleActive, entry.isIntersecting);
+
+                if (entry.isIntersecting && !entry.target.classList.contains('fullpage')) {
+                    currentSection = index;
+                }
+            });
+        },
+        {
+            threshold: 0.8,
+        }
     );
 
     sections.forEach((section) => observer.observe(section));
-  });
-  
+});
+
 window.addEventListener("wheel", (event) => {
-    if(isScrolling){
+    if (isScrolling) {
         event.preventDefault();
         return;
     }
-    if (sections.length === 0 || currentSection > sections.length - 1 ) return;
+    if (sections.length === 0 || currentSection > sections.length - 1) return;
 
     // ? Validamos que acción ejecuta el scroll
     const currentSectionElement = sections[currentSection];
@@ -68,10 +91,10 @@ window.addEventListener("wheel", (event) => {
     const sectionBottom = currentSectionElement.offsetTop + currentSectionElement.clientHeight;
 
     // Detectar la visibilidad de la siguiente sección
-    
+
     switch (getAction(event)) {
         case 'baja'://? Para ver el siguiente
-        const nextSectionVisible = scrollTop + window.innerHeight > sectionBottom - 100; // Ajusta el valor de 100 según sea necesario
+            const nextSectionVisible = scrollTop + window.innerHeight > sectionBottom - 100; // Ajusta el valor de 100 según sea necesario
             const puedeSaltar = nextSectionVisible && currentSection < sections.length - 1 &&
                 (sections[currentSection + 1].classList.contains('fullpage') ||
                     (!sections[currentSection + 1].classList.contains('fullpage') &&
@@ -93,6 +116,6 @@ window.addEventListener("wheel", (event) => {
             }
             break;
     }
-   
+
     return;
-},{passive: false});
+}, { passive: false });
