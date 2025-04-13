ecfdb29837f0c23e536b75d9f88e69d78ec12e6b
José Argüello <argelloestigarribia.josantonio@gmail.com>
2025-04-12 21:04:51 -0400

    ajustar visibilidad del observer, para que entre en el observer mientras se muestra hasta el 50% de la pantalla

--- src/scripts/showSection.js
+++ src/scripts/showSection.js
@@ -1,5 +1,5 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init();
 
@@ -8,134 +8,150 @@ let isScrolling = false;
 
 const sections = document.querySelectorAll("section");
 //? Para topBar
-const styleActive = 'bg-white/15';
+const styleActive = "bg-[#10B981]/50"; //'bg-white/15';
 const topbar = document.querySelector("#topbar");
 
 //? Si no tiene hash ir siempre arriba
 if (!document.location.hash?.length) {
-    document.documentElement.scrollIntoView({ top: 0 });
+  document.documentElement.scrollIntoView({ top: 0 });
 } else {
-    const index = Array.from(sections).findIndex(
-        item => '#' + item.id === document.location.hash && item.classList.contains('fullpage')
-    );
-    // Se registra en el current index para que
-    // Funcione los eventos al cambio de hash
-    if (index >= 0) {
-        currentSection = index;
-        scrollToSection(index);
-    }
+  const index = Array.from(sections).findIndex(
+    (item) =>
+      "#" + item.id === document.location.hash &&
+      item.classList.contains("fullpage")
+  );
+  // Se registra en el current index para que
+  // Funcione los eventos al cambio de hash
+  if (index >= 0) {
+    currentSection = index;
+    scrollToSection(index);
+  }
 }
 
 //? Para no mostrar #inicio
-window.addEventListener('hashchange', async function (a) {
-    const hash = document.location.hash; 
-    if (hash == '#inicio') {
-        currentSection = 0;
-        history.replaceState(null, '', '/');
-        scrollToSection(0);
-        return;
-    }
+window.addEventListener("hashchange", async function (a) {
+  const hash = document.location.hash;
+  if (hash == "#inicio") {
+    currentSection = 0;
+    history.replaceState(null, "", "/");
+    scrollToSection(0);
+    return;
+  }
 
-    const index = await Array.from(sections).findIndex(
-        item => '#' + item.id === hash && item.classList.contains('fullpage')
-    );
-    if (index >= 0) {
-        currentSection = index;
-        scrollToSection(index);
-    }
-
+  const index = await Array.from(sections).findIndex(
+    (item) => "#" + item.id === hash && item.classList.contains("fullpage")
+  );
+  if (index >= 0) {
+    currentSection = index;
+    scrollToSection(index);
+  }
 });
 
 function scrollToSection(index) {
-    if (index >= 0 && index < sections.length) {
-        if (index == 0) {
-            document.documentElement.scrollIntoView({
-                top: 0,
-                behavior: "smooth"
-            });
-        } else {
-            sections[index].scrollIntoView({ behavior: "smooth" });
-        }
-        setTimeout(() => {
-            isScrolling = false;
-        }, 200);
+  if (index >= 0 && index < sections.length) {
+    if (index == 0) {
+      document.documentElement.scrollIntoView({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      sections[index].scrollIntoView({ behavior: "smooth" });
     }
+    setTimeout(() => {
+      isScrolling = false;
+    }, 200);
+  }
 }
 
 function getAction(event) {
-    if (event.deltaY > 0) {
-        return 'baja';
-    } else if (event.deltaY < 0) {
-        return 'sube';
-    }
+  if (event.deltaY > 0) {
+    return "baja";
+  } else if (event.deltaY < 0) {
+    return "sube";
+  }
 }
 
 function calcularSeccion() {
-    sections.forEach((section, index) => {
-        if (section.getBoundingClientRect()) currentSection = index;
-    })
+  sections.forEach((section, index) => {
+    if (section.getBoundingClientRect()) currentSection = index;
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                const index = Array.from(sections).indexOf(entry.target); // Obtén el índice de la sección
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const index = Array.from(sections).indexOf(entry.target); // Obtén el índice de la sección
 
-                //? Esto es para activar el topbar
-                topbar.children[index].classList.toggle(styleActive, entry.isIntersecting);
+        //? Esto es para activar el topbar
+        topbar.children[index].classList.toggle(
+          styleActive,
+          entry.isIntersecting
+        );
 
-                if (entry.isIntersecting && !entry.target.classList.contains('fullpage')) {
-                    currentSection = index;
-                }
-            });
-        },
-        {
-            threshold: 0.8,
+        if (
+          entry.isIntersecting &&
+          !entry.target.classList.contains("fullpage")
+        ) {
+          currentSection = index;
         }
-    );
+      });
+    },
+    {
+      threshold: 0.5,
+    }
+  );
 
-    sections.forEach((section) => observer.observe(section));
+  sections.forEach((section) => observer.observe(section));
 });
 
-window.addEventListener("wheel", (event) => {
+window.addEventListener(
+  "wheel",
+  (event) => {
     if (isScrolling) {
-        event.preventDefault();
-        return;
+      event.preventDefault();
+      return;
     }
     if (sections.length === 0 || currentSection > sections.length - 1) return;
 
     // ? Validamos que acción ejecuta el scroll
     const currentSectionElement = sections[currentSection];
     const scrollTop = window.scrollY || window.pageYOffset;
-    const sectionBottom = currentSectionElement.offsetTop + currentSectionElement.clientHeight;
+    const sectionBottom =
+      currentSectionElement.offsetTop + currentSectionElement.clientHeight;
 
     // Detectar la visibilidad de la siguiente sección
 
     switch (getAction(event)) {
-        case 'baja'://? Para ver el siguiente
-            // const nextSectionVisible = scrollTop + window.innerHeight > sectionBottom - 100; // Ajusta el valor de 100 según sea necesario
-            const puedeSaltar =  currentSection < sections.length - 1 &&
-                (sections[currentSection + 1].classList.contains('fullpage') ||
-                    (!sections[currentSection + 1].classList.contains('fullpage') &&
-                        sections[currentSection].classList.contains('fullpage')));
+      case "baja": //? Para ver el siguiente
+        // const nextSectionVisible = scrollTop + window.innerHeight > sectionBottom - 100; // Ajusta el valor de 100 según sea necesario
+        const puedeSaltar =
+          currentSection < sections.length - 1 &&
+          (sections[currentSection + 1].classList.contains("fullpage") ||
+            (!sections[currentSection + 1].classList.contains("fullpage") &&
+              sections[currentSection].classList.contains("fullpage")));
 
-            if (puedeSaltar) {
-                isScrolling = true;
-                currentSection++;
-                scrollToSection(currentSection);
-                event.preventDefault();
-            }
-            break;
-        case 'sube':
-            if (currentSection > 0 && sections[currentSection - 1].classList.contains('fullpage')) {
-                isScrolling = true;
-                currentSection--;
-                scrollToSection(currentSection);
-                event.preventDefault();
-            }
-            break;
+        if (puedeSaltar) {
+          isScrolling = true;
+          currentSection++;
+          scrollToSection(currentSection);
+          event.preventDefault();
+        }
+        break;
+      case "sube":
+        if (
+          currentSection > 0 &&
+          sections[currentSection - 1].classList.contains("fullpage")
+        ) {
+          isScrolling = true;
+          currentSection--;
+          scrollToSection(currentSection);
+          event.preventDefault();
+        }
+        break;
     }
 
     return;
-}, { passive: false });
+  },
+  { passive: false }
+);
